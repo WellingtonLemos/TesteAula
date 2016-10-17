50b28e564129db1c445af8a26620c61ee611061e
WellingtonLemos <Wellington@10.0.254.79>
2016-10-17 10:16:41 -0200

    Nova modificação.

--- src/Teste/Aula/br/WellingtonLemos.java
+++ src/Teste/Aula/br/WellingtonLemos.java
@@ -1,5 +1,11 @@
 package Teste.Aula.br;
 
 public class WellingtonLemos {
-
+	public static void main(String[]args){
+		
+		int num1 = 3, num2=4;
+		int soma = num1 + num2;
+		System.out.println(soma);
+		
+	}
 }
